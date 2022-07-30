b8c27e728578b5852f3828c809e03b55a61fd7a5
Ahad <ahadsheikh@gmail.com>
2022-07-31 02:16:03 +0530

    31July by Ahad

--- src/Components/Pages/OnlineBanking.jsx
+++ src/Components/Pages/OnlineBanking.jsx
@@ -1,33 +1,18 @@
-import React, {useState} from "react";
-import {Tabs,Tab} from "@mui/material";
-import Addbank from "./Addbank";
-import ShowBanks from "./ShowBanks";
-import Deposit from "./Deposit";
-// import ShowItems from "./ShowItems";
+import React from 'react'
+import {Link} from 'react-router-dom'
+// import {AddBank} from './OnlineBanking/Addbank';
 
-function OnlineBanking(){
-  const [val,setVal]=useState("addbank");
 
-  return(
 
-    <div>
-      <h3>online banking - diffrent tabs to explore</h3>
-      <Tabs value={val} onChange={(e,val)=> setVal(val)}>
-        <Tab value="addbank" label="Add Bank"/>
-        <Tab value="showbank" label="Show Bank"/>
-        <Tab value="deposit" label="Deposit"/>
-        {/* <Tab value="showitems" label="ShowItems"/> */}
-        
-      </Tabs>
 
-      {val==="addbank" && <Addbank/>}
-      {val==="showbank" && <ShowBanks/>}
-      {val==="deposit" && <Deposit/>}
-    
+function OnlineBanking() {
+  return (
+    <div className='NavTab'>
+            {/* <li><Link to="/onlinebanking/addbank">AddBank</Link></li> */}
+            <li><Link to="/onlinebanking/debit">Debit</Link></li>
 
     </div>
   )
 }
 
 export default OnlineBanking
-
