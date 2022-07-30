2dbda7ad47092d893df08e1216a4a6be05bf9a52
anamsiddiqui-07 <asra.anam94@gmail.com>
2022-07-30 15:11:14 +0530

    3othjuly_asra

--- src/Components/Pages/OnlineBanking.jsx
+++ src/Components/Pages/OnlineBanking.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-// import {AddBank} from './OnlineBanking/Addbank';
+import React, {useState} from "react";
+import {Tabs,Tab} from "@mui/material";
+import Addbank from "./Addbank";
+import ShowBanks from "./ShowBanks";
+import Debit from "./Debit";
 
+function OnlineBanking(){
+  const [val,setVal]=useState("addbank");
 
+  return(
 
+    <div>
+      <h3>online banking - diffrent tabs to explore</h3>
+      <Tabs value={val} onChange={(e,val)=> setVal(val)}>
+        <Tab value="addbank" label="Add Bank"/>
+        <Tab value="showbank" label="Show Bank"/>
+        <Tab value="debit" label="Debit"/>
+      </Tabs>
 
-function OnlineBanking() {
-  return (
-    <div className='NavTab'>
-            {/* <li><Link to="/onlinebanking/addbank">AddBank</Link></li> */}
-            <li><Link to="/onlinebanking/debit">Debit</Link></li>
+      {val==="addbank" && <Addbank/>}
+      {val==="showbank" && <ShowBanks/>}
+      {val==="debit" && <Debit/>}
 
     </div>
   )
 }
 
 export default OnlineBanking
+
